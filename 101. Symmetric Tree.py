7dbc51d50966b4e1a8fbc12e8558c32e458fdaa5
Pongsakorn <107350719+ouhzzzooo@users.noreply.github.com>
2023-11-16 22:04:00 +0700

    recursive
    check left of left root and right of right root if it not none then true but if it different then false because of symmetric and checking value of it

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        if root == None:
+            return True
+        return self.isSame(root.left, root.right)
+        
+    def isSame(self, leftroot, rightroot):
+
+        if leftroot == None and rightroot == None:
+            return True
+        if rightroot == None or leftroot == None:
+            return False
+        if leftroot.val != rightroot.val:
+            return False
+
+        else:
+            return (self.isSame(leftroot.left, rightroot.right)) and (self.isSame(leftroot.right, rightroot.left))
